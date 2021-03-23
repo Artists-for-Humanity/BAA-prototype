5db62cfc8290f49aeeba1b032d141d11284a004e
Junqiang Mai <junqiangmai2004@gmail.com>
2021-03-23 17:29:57 -0400

    php for admission

---
+++ site/hero.php/index.php
@@ -0,0 +1,11 @@
+<?php template("hero.php", array('text' => 'Example Page')) ?>
+<nav><a href="">Home</a><a href="/about">About</a></nav>
+  <div class="banner">
+    <div class="admission-title">
+    <?php print $Admission; ?>
+    </div>
+    <p class="header-text">Jodie McSally Dance Program Class of '21</p>
+    <div class="placeholder-image">
+      <img src="../assets/images/placeholder-image-rectangle.png" alt="placeholder-image-rectangle">
+    </div>
+  </div>
