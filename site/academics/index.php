10982c964214b09fbca7011c474dae1f73329e66
JeremiahHarris19 <75698932+JeremiahHarris19@users.noreply.github.com>
2021-03-23 17:05:57 -0400

    moved header into php file

--- site/academics/index.php
+++ site/academics/index.php
@@ -13,15 +13,7 @@
 <body>
   <nav><a href="/">Home</a><a href="/about">About</a></nav>
   <div>
-    <div class="header">
-      <div class="header-block-text">Academics</div>
-      <div class="header-text">Jodie Mcsally<br>Dance Program, Class of 21</div>
-      <picture>
-        <source srcset="/assets/images/placeholder-image-rectangle-tall.png"
-          media="(orientation: portrait) and ( min-width: 600px)">
-        <img src="/assets/images/placeholder-image-rectangle.png" alt="image" />
-      </picture>
-    </div>
+    <?php include "hero.php" ?>
     <div class="sub-nav">
       <div>Majors</div>
       <div>Inquiry & innovation</div>
