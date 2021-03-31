5af76233145486236a199d68605fd3561ca0c02d
Stephen Cronin <638332+cronin4392@users.noreply.github.com>
2021-03-31 12:52:05 -0400

    Move example page data into its own file (#25)
    * Move example page data into its own file
    
    * Set max width on hero in example page

--- site/example/templates/three-up-item.php
+++ site/example/templates/three-up-item.php
@@ -1,10 +1,16 @@
 <a class="grid-item" href="#">
   <div class="grid-item-image">
     <img src="/assets/images/placeholder-image-rectangle.png" alt="Image" />
+    <?php if ($month || $day) { ?>
     <div class="grid-item-date">
+      <?php if ($month) { ?>
       <span class="month"><?php print $month; ?></span>
+      <?php } ?>
+      <?php if ($day) { ?>
       <span class="day"><?php print $day; ?></span>
+      <?php } ?>
     </div>
+    <?php } ?>
   </div>
   <div class="grid-item-title"><?php print $text; ?></div>
 </a>
