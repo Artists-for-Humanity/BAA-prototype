d30260aabf2404e3a4e206a1c262329a7926c934
Jamie D <itisjamied@gmail.com>
2021-04-01 15:26:44 -0400

    create data-2.php

---
+++ site/example/data-2.php
@@ -0,0 +1,63 @@
+<?php 
+  $navItems = array(
+    array('name' => 'About', 'link' => '/about'),
+    array('name' => 'Academics', 'link' => '/academics'),
+    array('name' => 'Admission', 'link' => '/admission'),
+    array('name' => 'Dance', 'link' => '/dance'),
+  );
+
+  $pageContent = array(
+    array(
+      "template" => "templates/hero.php",
+      "data" => array(
+        'text' => 'Example Page 2'
+      )
+    ),
+    array(
+      "template" => "templates/three-up.php",
+      "data" => array(
+        "title" => "Upcoming Events",
+        "items" => array(
+          array(
+            'month' => 'Feb',
+            'day' => '10',
+            'text' => 'Event name 1'
+          ),
+          array(
+            'month' => 'Mar',
+            'day' => '15',
+            'text' => 'Event name 2'
+          ),
+          array(
+            'month' => 'Mar',
+            'day' => '22',
+            'text' => 'Event name 3'
+          )
+        )
+      )
+    ),
+    array(
+      "template" => "templates/three-up.php",
+      "data" => array(
+        "title" => "Latest News",
+        "items" => array(
+          array(
+            'text' => 'News name 1'
+          ),
+          array(
+            'text' => 'News name 2'
+          ),
+          array(
+            'text' => 'News name 3'
+          )
+        )
+      )
+    ),
+    array(
+      "template" => "templates/hero.php",
+      "data" => array(
+        'text' => 'Cool stuff'
+      )
+    ),
+  );
+?>
