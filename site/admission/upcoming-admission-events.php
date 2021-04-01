f1006465500496397e6ebec748ee685e445c1ca4
Junqiang Mai <junqiangmai2004@gmail.com>
2021-04-01 18:05:58 -0400

    php files

---
+++ site/admission/upcoming-admission-events.php
@@ -0,0 +1,10 @@
+<a class="grid-item" href="<?php print $link;?>">
+  <div class="grid-item-image">
+    <img src="/assets/images/placeholder-image-rectangle.png" alt="Image" />
+    <div class="grid-item-date">
+      <span class="month"><?php print $month; ?></span>
+      <span class="day"><?php print $day; ?></span>
+    </div>
+  </div>
+  <div class="grid-item-title"><?php print $text; ?></div>
+</a>
